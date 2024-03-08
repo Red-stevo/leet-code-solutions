c57fe97335302db9ff0f1e36e788798526456489
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-08 20:19:11 +0300

    comfigured the partitioning of the array

--- QuickSort/src/IQuickSort.java
+++ QuickSort/src/IQuickSort.java
@@ -1,6 +1,6 @@
 public interface IQuickSort {
 
-    void partition(int start, int end);
+    int partition(int start, int end);
 
     int[] quickSort(int start, int end);
 }
--- QuickSort/src/QuickSortImpl.java
+++ QuickSort/src/QuickSortImpl.java
@@ -8,13 +8,30 @@ public class QuickSortImpl implements IQuickSort{
     }
 
     @Override
-    public void partition(int start, int end) {
+    public int partition(int start, int end) {
+        int pivot = end;
+        int temp;
 
+        if(start < end){
+            while (arr[start] < arr[end]){
+                start++;
+            }
+            while (arr[end] >= arr[end]){
+                end--;
+            }
+            temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+        }
+        temp = arr[pivot];
+        arr[pivot] = arr[end];
+        arr[end] = temp;
+        return end;
     }
 
     @Override
     public int[] quickSort(int start, int end) {
-
+        partition(start, end);
 
         return arr;
     }
