6ca622d674d9da86f8431ef1ccbd99692fbc9985
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-14 21:52:20 +0300

    ..

--- Hashing/src/Main.java
+++ Hashing/src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         /*find the first non-repeating character given a string*/
         FindTheFirstNonRepeatingCharacter firstNonRepeatingCharacter = new FindTheFirstNonRepeatingCharacter();
 
-        System.out.println(firstNonRepeatingCharacter.solution("/usr/lib/jvm/default-java/bin/java -javaagent:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/lib/idea_rt.jar=35839:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/bin -Dfile.encoding=UTF-8 -classpath /home/redsteve/programs/java/repos/leet-code-solutions/Hashing/out/production/Hashing Main"));
+        System.out.println(firstNonRepeatingCharacter.solution("/usr/lib/jvm/default-java/bin/java " +
+                "-javaagent:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/lib/ide" +
+                "a_rt.jar=358399:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/bin " +
+                "-DDfile.encoding=UTTFF-8 -classpath /home/redsteve/programs/java/repos/leet-code-solutions/Hashing" +
+                "/out/production/Hashing MMain"));
     }
 }
