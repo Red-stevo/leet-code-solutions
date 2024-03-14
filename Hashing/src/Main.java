34e89aa820b91e09ee457b5412ed6f806dfbe9cf
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-14 15:07:28 +0300

    well working

--- Hashing/src/Main.java
+++ Hashing/src/Main.java
@@ -3,5 +3,11 @@ public class Main {
 
 
         //test  the hashing algorithms
+
+
+        /*find the first non-repeating character given a string*/
+        FindTheFirstNonRepeatingCharacter firstNonRepeatingCharacter = new FindTheFirstNonRepeatingCharacter();
+
+        System.out.println(firstNonRepeatingCharacter.solution("/usr/lib/jvm/default-java/bin/java -javaagent:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/lib/idea_rt.jar=35839:/home/redsteve/Public/iso files/installation_files/idea-IU-233.13135.103/bin -Dfile.encoding=UTF-8 -classpath /home/redsteve/programs/java/repos/leet-code-solutions/Hashing/out/production/Hashing Main"));
     }
 }
