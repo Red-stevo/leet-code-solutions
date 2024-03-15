46e590507cc6b4767b588a670f9cc430a9ba751c
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-15 12:32:11 +0300

    done

--- SmallestValueOfANumber/src/Main.java
+++ SmallestValueOfANumber/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SmallestValueOfNumber smallestValueOfNumber = new SmallestValueOfNumber();
 
-        smallestValueOfNumber.solution(31980276522L);
+        System.out.print(smallestValueOfNumber.solution(474447278687L));
     }
 }
--- SmallestValueOfANumber/src/SmallestValueOfNumber.java
+++ SmallestValueOfANumber/src/SmallestValueOfNumber.java
@@ -11,8 +11,10 @@ public class SmallestValueOfNumber {
 
         arr = quickSort(arr,0,arr.length-1);
 
-        for(int i : arr){
-            System.out.print(i +"\t");
+        number = 0;
+
+        for(int length = arr.length -1; length >= 0; --length){
+            number += ((long) arr[1] * (10^length));
         }
 
         return number;
@@ -20,8 +22,13 @@ public class SmallestValueOfNumber {
 
     private int[] quickSort(int[] arr, int start, int end){
 
+        if(start >= end) return arr;
+
         int pivot = partition(arr,start,end);
 
+        quickSort(arr,start,pivot-1);
+
+        quickSort(arr,pivot+1, end);
 
         return arr;
     }
@@ -33,11 +40,11 @@ public class SmallestValueOfNumber {
 
         while(start < stop){
 
-            while(start < stop && arr[start] <= pivot){
+            while(start < stop && arr[start] < pivot){
                 arr[start-1]  = arr[start];
                 start++;
             }
-            while(stop > start && arr[stop] > pivot){
+            while(stop > start && arr[stop] >= pivot){
                 stop--;
             }
             if(start < stop){
@@ -47,6 +54,21 @@ public class SmallestValueOfNumber {
         arr[start-1] = pivot;
         return start-1;
     }
+
+    private int partition2(int[] arr, int start, int end){
+        int pointer1 = start -1, pointer2;
+        for(pointer2 = start ; pointer2  < end; pointer2++){
+
+            if(arr[pointer2] < arr[end]){
+                ++pointer1;
+                swap(arr,pointer1,pointer2);
+            }
+            pointer1++;
+            swap(arr,pointer1,end);
+        }
+        return pointer1;
+    }
+
 
     private void swap(int[] arr, int point1, int point2){
         int temp = arr[point1];
