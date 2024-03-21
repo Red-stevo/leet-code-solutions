857dd282d80f542cc24635b3e4570864b0a854d5
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-21 06:09:00 +0300

    done

--- SmallestArrangment/src/Main.java
+++ SmallestArrangment/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SmallestArrangement smallestArrangement = new SmallestArrangement();
 
-        Long num = smallestArrangement.arrange(12345678765L);
+        Long num = smallestArrangement.arrange(-4623153325532160L);
 
         System.out.println(num);
     }
