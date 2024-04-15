c4b385304d63cb96c641e927e6685e9dca53a1dc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-15 08:48:22 +0300

    configuring the method functionality

---
+++ CheckPalidrome/src/PalindromeCheck.java
@@ -0,0 +1,18 @@
+public class PalindromeCheck {
+
+    public Boolean check(String word){
+
+        /*variable used in the code*/
+        int end = word.length() -1 ; //this is the pointer to the last character in the word(also in the char array);
+
+        /*Convert the word into an array of characters*/
+        char[] wordArray = word.toCharArray();
+
+        /*Loop throw the character to determine whether it is an array*/
+        for (int i = 0 ; i < end ; i++){
+            int j = end - i;
+        }
+
+        return  null;
+    }
+}
