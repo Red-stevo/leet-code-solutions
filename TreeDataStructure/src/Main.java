2badfb1f232f5769993084615abc90dfe40be17f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-16 17:02:21 +0300

    Commit message >>>>> creates the method to handle the nodes creation <<<<<

--- TreeDataStructure/src/Main.java
+++ TreeDataStructure/src/Main.java
@@ -1,15 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final TreeNode root = new TreeNode("Root");
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    private static void createBinaryTree(String data){
+
+         // This node will be used in the creation process of the tree
+        TreeNode tempNode;
+
+        TreeNode newNode = new TreeNode(data);
+
+        tempNode = root;
+
+        while (true) {
+            if (tempNode.getLeft() == null) {
+                tempNode.setLeft(newNode);
+                break;
+            } else if (tempNode.getRight() == null) {
+                tempNode.setRight(newNode);
+                break;
+            } else if (tempNode.getLeft().getLeft() == null || tempNode.getLeft().getRight() == null) {
+                tempNode = tempNode.getLeft();
+            } else if (tempNode.getRight().getLeft() == null || tempNode.getRight().getRight() == null) {
+                tempNode = tempNode.getRight();
+            }
         }
     }
 }
--- TreeDataStructure/src/TreeNode.java
+++ TreeDataStructure/src/TreeNode.java
@@ -5,4 +5,32 @@ public class TreeNode {
     private TreeNode right;
 
     private String data;
+
+    public TreeNode(String data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
