9f9371e736275b1cd853dbaf98cc8a93ee61cfe5
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-19 11:08:43 +0300

    Commit message >>>>> creating the queue for the traversal <<<<<

---
+++ SmallestStringFromLeaf/src/SmallestString.java
@@ -0,0 +1,18 @@
+import java.util.Queue;
+
+public class SmallestString {
+
+    /*To solve this problem I will implement level order tree traversal
+     *for this :-
+     *1. I will need to implement a queue.
+     *2.
+     */
+
+
+    private TreeNode[] queue = new TreeNode[100];
+
+
+
+}
+
+
