892e248bf894d0d872942df619e25a5dfcc77798
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-07 20:04:27 +0300

    Commit message >>>>> bug fixed. <<<<<

--- MedianValue/src/Solution.java
+++ MedianValue/src/Solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int sumLength = (nums1.length + nums2.length)-1, i=0, j=0,pos1=-1,pos2=0,pos3=0;
+        boolean check = true;
         if(nums1.length == 0 && nums2.length == 1) return nums2[0];
         else if(nums1.length == 1 && nums2.length == 0) return nums1[0];
         if(sumLength%2 == 0) pos1 = sumLength/2;
@@ -15,46 +16,41 @@ public class Solution {
             }
         }
 
-        System.out.println(pos2);
-        System.out.println(pos3);
         for (int k = 0; k < sumLength; k++) {
             if (i >= nums1.length) {
                 if (k == pos1) return nums2[j];
-                else if (k == pos2) pos2 = nums2[j];
-                else if (k == pos3) {
-                    pos3 = nums2[j];
-                    break;
+                else if (k == pos2 && check){
+                    pos2 = nums2[j];
+                    check = false;
                 }
+                else if (k == pos3) return (double) (pos2 + nums2[j])/2;
                 j++;
             } else if (j >= nums2.length) {
                 if (k == pos1) return (nums1[i]);
-                else if (k == pos2) pos2 = nums1[i];
-                else if (k == pos3) {
-                    pos3 = nums1[i];
-                    break;
+                else if (k == pos2 && check){
+                    pos2 = nums1[i];
+                    check = false;
                 }
+                else if (k == pos3) return (double) (pos2 + nums2[i])/2;
                 i++;
             } else if (nums1[i] < nums2[j]) {
                 if (k == pos1) return (nums1[i]);
-                else if (k == pos2) pos2 = nums1[i];
-                else if (k == pos3) {
-                    pos3 = nums1[i];
-                    break;
+                else if (k == pos2 && check){
+                    pos2 = nums1[i];
+                    check = false;
                 }
+                else if (k == pos3) return (double) (pos2 + nums2[i])/2;
                 i++;
-            } else if (nums1[i] >= nums2[j]) {
+            } else {
                 if (k == pos1) return (nums2[j]);
-                else if (k == pos2) pos2 = nums2[j];
-                else if (k == pos3) {
-                    pos3 = nums2[j];
-                    break;
+                else if (k == pos2 && check){
+                    pos2 = nums2[j];
+                    check = false;
                 }
+                else if (k == pos3) return (double) (pos2 + nums2[j])/2;
                 j++;
             }
         }
-
-        System.out.println(pos2);
-        System.out.println(pos3);
-        return (double) (pos2 + pos3)/2;
+        return 0;
     }
 }
