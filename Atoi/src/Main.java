bc830d39ce740b08a4a0c5ee7e0bbe5ad3504d7e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-21 12:30:48 +0300

    Commit message >>>>> bugs here and there <<<<<

--- Atoi/src/Main.java
+++ Atoi/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.myAtoi("         0-15432345676543456765434567654345678765434567876523-w23ht"));
+        System.out.println(solution.myAtoi("0-1"));
     }
 }
--- Atoi/src/Solution.java
+++ Atoi/src/Solution.java
@@ -4,28 +4,23 @@ public class Solution {
         char[] chars = s.toCharArray();
         int j=0, i;
         boolean checkZero = false;
-        char sign = '+';
         for (i = 0; i < chars.length; i++) {
             if((chars[i] == '0' || chars[i] == ' ') && i == j) {
                 if(chars[i] == '0') checkZero = true;
                 ++j;
                 continue;
             }
-            if((chars[i] == '-' || chars[i] == '+') && j == i && !checkZero) {
-                ++j;
-                sign = chars[i];
-                continue;
-            }
+            if((chars[i] == '-' || chars[i] == '+') && j == i && !checkZero) continue;
+
             if(!(chars[i] >= '0' && chars[i] <= '9')) break;
         }
         s = s.substring(j,i);
-        if(s.isEmpty()) return 0;
-        s = sign+s;
+        if(s.equals("-") || s.equals("+") || s.isEmpty()) return 0;
         try{
             i = Integer.parseInt(s);
         }catch(NumberFormatException e){
-            if(sign == '-') return -2147483648;
-            return -2147483648;
+            if(chars[j] == '-') return -2147483648;
+            return 2147483647;
         }
         return i;
     }
