da477177de4db1cdcaead98a71f1d107c71cfe7f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-22 21:35:45 +0300

    Commit message >>>>> done <<<<<

--- Atoi/src/Main.java
+++ Atoi/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        System.out.println(solution.myAtoi("000000000000000000"));
+        System.out.println(solution.myAtoi("0000000000000000001erre"));
     }
 }
