f4cb556b20e6c83b861daac17a8232b2b5f42dbc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-23 11:18:51 +0300

    Commit message >>>>> first attempt done <<<<<

--- HeapAndHeapSort/Heaps/Heapify.py
+++ HeapAndHeapSort/Heaps/Heapify.py
@@ -1,13 +1,29 @@
 class Heap:
     def min_heap(self, nums):
         size = len(nums) - 1
+        LENGTH = size
 
         while size >= 0:
             parent = (size - 1) / 2
             if nums[parent] > nums[size]:
                 self.swap(nums, parent, size)
-            else:
-                size -= 1
+            size2 = size
+            left_child = 2 * size2 + 1
+            right_child = left_child + 1
+            while left_child <= LENGTH:
+                if left_child <= LENGTH and right_child <= LENGTH:
+                    if nums[left_child] < nums[right_child] and nums[left_child] < nums[size2]:
+                        self.swap(nums, left_child, size2)
+                        size2 = left_child
+                    elif nums[right_child] < nums[size2]:
+                        self.swap(nums, right_child, size2)
+                        size2 = right_child
+                elif nums[left_child] < nums[size2]:
+                    self.swap(nums, left_child, size2)
+                    size2 = left_child
+                left_child = 2 * size2 + 1
+                right_child = left_child + 1
+            size -= 1
 
     def swap(self, nums, index1, index2):
         temp = nums[index1]
