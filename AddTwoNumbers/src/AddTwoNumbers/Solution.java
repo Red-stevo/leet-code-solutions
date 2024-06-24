915738a13b42f6e0105dc6f3e2e330179a034d56
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-24 21:06:23 +0300

    Commit message >>>>> basic setup <<<<<

---
+++ AddTwoNumbers/src/AddTwoNumbers/Solution.java
@@ -0,0 +1,11 @@
+package AddTwoNumbers;
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        while(l1.next != null || l2.next != null){
+
+            if()
+        }
+    }
+}
