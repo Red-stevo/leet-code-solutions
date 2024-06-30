e27af52e75008fc7dc053155a27685891a9a2efe
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-30 17:53:49 +0300

    Commit message >>>>> Done <<<<<

--- InsertionPosition/src/Main.java
+++ InsertionPosition/src/Main.java
@@ -3,6 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = {20,34,45,78,86,99,107,110,123,134};
+        int num = 89;
         Solution solution = new Solution();
+
+        int index = solution.insertNum(arr,num);
+        System.out.println("The position to insert the element is: " + index);
+
     }
 }
--- InsertionPosition/src/Solution.java
+++ InsertionPosition/src/Solution.java
@@ -2,7 +2,13 @@ public class Solution {
 
     //method for inserting an element in an array
     public int insertNum(int[] arr,int num){
-
+        for(int i = 0;i< arr.length; i++){
+            if(arr[i] < num){
+                continue;
+            }
+            return i;
+        }
+        return arr.length;
     }
 
 }
