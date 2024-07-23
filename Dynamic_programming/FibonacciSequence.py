615149bc7edd2e488b8989c3344c1de63d3a5992
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-23 06:42:55 +0300

    Commit message >>>>> dynamic fibonacci sequence algorithm. <<<<<

--- Dynamic_programming/FibonacciSequence.py
+++ Dynamic_programming/FibonacciSequence.py
@@ -1,5 +1,8 @@
 def main():
-    print(f"fibonacci of 5 {fibonacci_sequence(5)}")
+    n = 50
+    memo = [None] * (n+1)
+    print(f"fibonacci of {n} is {fibonacci_dynamically(n,memo)}")
+    print(f"fibonacci of {n} is {fibonacci_sequence(n)}")
 
 
 def fibonacci_sequence(n):
@@ -8,6 +11,22 @@ def fibonacci_sequence(n):
     if n == 1 or n == 2:
         return 1
     return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
+
+
+def fibonacci_dynamically(n, memoization):
+
+    if memoization[n] is not None:
+        return memoization[n]
+
+    if n == 0:
+        return 0
+
+    if n == 1 or n == 2:
+        return 1
+
+    memoization.insert(n, fibonacci_dynamically(n-1, memoization) + fibonacci_dynamically(n-2, memoization))
+
+    return memoization[n]
 
 
 if __name__ == "__main__":
