d5a0a7206b4ebc061317f40a0e5103d6edaa559e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-24 20:32:00 +0300

    Commit message >>>>> base case setup done <<<<<

--- CoinChange/src/Main.java
+++ CoinChange/src/Main.java
@@ -4,9 +4,22 @@ public class Main {
 
     }
 
-    public static int coinChange(){
+    public static int coinChange(int[] coins, int amount){
 
+        //working array.
+        int[][] working_arr = new int[coins.length + 1][amount + 1];
 
+        for(int i = 0; i < coins.length; i++)
+            working_arr[i][0] = 0;
+
+        for (int i = 0; i < coins.length + 1; i++) {
+            for (int j = 0; j < amount + 1; j++){
+                if(coins[i] > i)
+                    working_arr[i][j] = 0;
+                else
+
+            }
+        }
         return 0;
     }
 }
