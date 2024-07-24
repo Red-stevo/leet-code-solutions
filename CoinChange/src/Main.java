184531149abe786e5f47d2e285104dc55f90bb18
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-24 20:40:03 +0300

    Commit message >>>>> setting the min value. <<<<<

--- CoinChange/src/Main.java
+++ CoinChange/src/Main.java
@@ -12,12 +12,12 @@ public class Main {
         for(int i = 0; i < coins.length; i++)
             working_arr[i][0] = 0;
 
-        for (int i = 0; i < coins.length + 1; i++) {
-            for (int j = 0; j < amount + 1; j++){
+        for (int i = 1; i < coins.length + 1; i++) {
+            for (int j = 1; j < amount + 1; j++){
                 if(coins[i] > i)
                     working_arr[i][j] = 0;
                 else
-
+                    working_arr[i][j] = min(1 + working_arr[i][j - coins[i]], working_arr[i-1][j]);
             }
         }
         return 0;
