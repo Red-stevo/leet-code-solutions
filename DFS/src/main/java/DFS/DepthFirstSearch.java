50e0b48025d3e825534ea96221b154f83a6f3588
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-28 17:27:26 +0300

    Commit message >>>>> done with DFS <<<<<

--- DFS/src/main/java/DFS/DepthFirstSearch.java
+++ DFS/src/main/java/DFS/DepthFirstSearch.java
@@ -4,9 +4,15 @@ import java.util.Stack;
 
 public class DepthFirstSearch {
 
-    public void dfs(Node node){
+    public void dfs(Node root){
         Stack<Node> stack = new Stack<>();
-
-
+        stack.push(root);
+        while (!stack.isEmpty()){
+            Node node = stack.pop();
+            node.setVisited(true);
+            System.out.println(node.getData());
+            if(node.getChildren() != null)
+               stack.addAll(node.getChildren());
+        }
     }
 }
