33f27c514639a9e8a11a0458a1512a049c7beae1
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-01 19:52:43 +0300

    Commit message >>>>> error time <<<<<

--- TravellingSalesMan/src/main/java/org/example/Main.java
+++ TravellingSalesMan/src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 
 
+import SalesManProblem.Solution;
+
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        solution.solution();
     }
 }
