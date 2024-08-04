e64cd991536abc225bed8f5e2861ccfdad22dbcf
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-04 21:46:49 +0300

    Commit message >>>>> working on the union fond data structure. <<<<<

---
+++ disjoinedSet/disjointSet.py
@@ -0,0 +1,14 @@
+def main():
+    list = []
+
+    #     initialized the list.
+    for i in range(11):
+        list[i] = i
+
+    print(list)
+
+    print("\nEnter the node to be connected.")
+
+
+if __name__ == "__main__":
+    main()
