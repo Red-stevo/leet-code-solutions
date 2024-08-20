bcbde558ca25074ea7353c146765e0aa6690d319
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-20 22:05:29 +0300

    Commit message >>>>> all done, convertion done <<<<<

--- ArrayToLinkedList/src/main/java/Solution/ToLinkedList.java
+++ ArrayToLinkedList/src/main/java/Solution/ToLinkedList.java
@@ -5,13 +5,19 @@ public class ToLinkedList {
     public Node toLinkedList(int[] array){
 
         Node root = null;
+        Node temp = null;
 
         for (int i : array){
-
             Node node = new Node(i);
 
             if (root == null){
                 root = node;
+                node.setPreviousNode(null);
+                temp = node;
+            }else {
+                temp.setNextNode(node);
+                node.setPreviousNode(temp);
+                temp = temp.getNextNode();
             }
         }
         return root;
--- ArrayToLinkedList/src/main/java/org/example/Main.java
+++ ArrayToLinkedList/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
 
+import Solution.Node;
+import Solution.ToLinkedList;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 10;
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = new Random().nextInt(n);
+
+
+        ToLinkedList toLinkedList = new ToLinkedList();
+
+        Node root = toLinkedList.toLinkedList(array);
+
+        System.out.print("The Original Array : ");
+        for (int i : array){
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        System.out.print("The Linked List Elements : ");
+        while (root != null){
+            System.out.print(root.getData()+"\t");
+            root = root.getNextNode();
+        }
     }
 }
