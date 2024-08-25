05dc188ce7f9e9ed63b0edd58b8e20307ef064e6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-25 22:05:27 +0300

    Commit message >>>>> bruteforce algorithm all done <<<<<

--- MaxOnes/src/BruteForceSolution/Solution.java
+++ MaxOnes/src/BruteForceSolution/Solution.java
@@ -2,10 +2,17 @@ package BruteForceSolution;
 
 public class Solution {
 
-    public int maxOnes(int[][] max){
+    public int maxOnes(int[][] max, int length){
+        int value = 0, maxValue=0, index;
 
-        for (int i = 0; i < max.length; i++) {
-
+        for (int i = 0; i < length; i++) {
+            for (int j = 0;j < length ; j++) {
+                value += max[i][j];
+            }
+            if(value > maxValue){
+                maxValue = value;
+                index = i;
+            }
         }
         return 0;
     }
