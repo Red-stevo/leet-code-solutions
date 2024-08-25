01e35fa9b28c62383c165a1eef54abfd2ae3bafc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-25 20:01:12 +0300

    Commit message >>>>> working on the brute fro solution. <<<<<

---
+++ MaxOnes/src/BruteForceSolution/Solution.java
@@ -0,0 +1,12 @@
+package BruteForceSolution;
+
+public class Solution {
+
+    public int maxOnes(int[][] max){
+
+        for (int i = 0; i < max.length; i++) {
+
+        }
+        return 0;
+    }
+}
