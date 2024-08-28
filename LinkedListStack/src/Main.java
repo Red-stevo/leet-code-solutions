ec8a49300d73bf2ccdfde2980b5590ff3b23a9de
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-28 19:31:13 +0300

    Commit message >>>>> done and tested. <<<<<

--- LinkedListStack/src/Main.java
+++ LinkedListStack/src/Main.java
@@ -1,7 +1,21 @@
+import LinkedListStack.Stack;
+
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10000000; i++)
+            stack.push(random.nextInt(50));
+
+        System.out.println();
+
+        for (int i = 0; i < 100000; i++)
+            System.out.print(stack.pop()+",\t");
 
     }
 }
