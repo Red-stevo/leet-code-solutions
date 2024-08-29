5dbbc281a4ef919565bca79c279c182c1bedd89f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-29 06:29:04 +0300

    Commit message >>>>> enqueue method done <<<<<

--- LinkedListQueue/src/LinkedListQueue/Queue.java
+++ LinkedListQueue/src/LinkedListQueue/Queue.java
@@ -1,4 +1,21 @@
 package LinkedListQueue;
 
-public class Queue {
+public class Queue<T> {
+
+    private Node<T> firstIn = null;
+
+    private Node<T> lastIn = null;
+
+    public void enqueue(T data){
+        Node<T> node = new Node<>(data);
+        node.setNextNode(null);
+        node.setPreviousNode(null);
+
+        if (firstIn == null && lastIn == null){
+            firstIn = lastIn = node;
+            return;
+        }
+
+
+    }
 }
