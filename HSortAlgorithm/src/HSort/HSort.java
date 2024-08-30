d2f190e6be130d698a83ec4df2c5639f5a77a76c
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-30 13:38:02 +0300

    Commit message >>>>> setting up generic h-sort algorithm. <<<<<

---
+++ HSortAlgorithm/src/HSort/HSort.java
@@ -0,0 +1,13 @@
+package HSort;
+
+public class HSort<T> implements Comparable<T> {
+
+    public T[] hsort(){
+
+        return null;
+    }
+    @Override
+    public int compareTo(T t) {
+        return 0;
+    }
+}
