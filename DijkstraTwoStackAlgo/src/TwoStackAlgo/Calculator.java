71cb2a129269889ff163f33e3848b01080102616
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-30 07:03:05 +0300

    Commit message >>>>> settinig up the iteration. <<<<<

--- DijkstraTwoStackAlgo/src/TwoStackAlgo/Calculator.java
+++ DijkstraTwoStackAlgo/src/TwoStackAlgo/Calculator.java
@@ -1,5 +1,9 @@
 package TwoStackAlgo;
 
+import LinkedListStack.Stack;
+
+import java.util.logging.Logger;
+
 /**
  * The Algorithm performs the calculation give a string of the form
  *  (2*((3+4)+5))
@@ -17,6 +21,34 @@ package TwoStackAlgo;
 public class Calculator {
 
     public Double calculate(String arithmeticProblem){
+        Stack<Character> arithmeticStack = new Stack<>();
+        Stack<Integer> numbersStack = new Stack<>();
+
+        for (int i = 0; i < arithmeticProblem.length(); i++) {
+            if(arithmeticProblem.charAt(i) == '(' || arithmeticProblem.charAt(i) == ' ')
+                continue;
+            else if (arithmeticProblem.charAt(i) == '+' || arithmeticProblem.charAt(i) == '*'
+            || arithmeticProblem.charAt(i) == '/' || arithmeticProblem.charAt(i) == '-') {
+                arithmeticStack.push(arithmeticProblem.charAt(i));
+            } else if (arithmeticProblem.charAt(i) == ')') {
+
+            } else if (checkNumber(arithmeticProblem.charAt(i))) {
+
+            }
+        }
         return null;
+    }
+
+    private boolean checkNumber(char c) {
+        try {
+            Double.parseDouble(String.valueOf(c));
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Character.");
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
