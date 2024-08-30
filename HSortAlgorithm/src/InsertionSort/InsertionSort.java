4add7e8a664875e34e11f9d07431407b991f4bfe
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-30 20:51:05 +0300

    Commit message >>>>> worked on the insertion sort algorithm <<<<<

--- HSortAlgorithm/src/InsertionSort/InsertionSort.java
+++ HSortAlgorithm/src/InsertionSort/InsertionSort.java
@@ -4,13 +4,18 @@ public class InsertionSort<T extends Comparable<T>>   {
 
     public T[] insertionSort(T[] t){
 
-        for (int i = 0; i < t.length; i++) {
-            for (int j = i+1; j >= 0 ; j--) {
-                if (t[i].compareTo(t[j]) < 0){
-
-                }
+        for (int i = 0; i < t.length-1; i++) {
+            for (int j = i+1; j > 0 ; j--) {
+                if (t[j-1].compareTo(t[j]) > 0)
+                    swap(j-1, j, t);
             }
         }
-        return null;
+        return t;
+    }
+
+    private void swap(int i, int j, T[] t) {
+        T temp = t[i];
+        t[i] = t[j];
+        t[j] = temp;
     }
 }
--- HSortAlgorithm/src/Main.java
+++ HSortAlgorithm/src/Main.java
@@ -1,10 +1,26 @@
 import InsertionSort.InsertionSort;
 
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        InsertionSort<Double> insertionSort = new InsertionSort<Double>();
+
+       InsertionSort<Integer> insertionSort = new InsertionSort<Integer>();
+        Random random = new Random();
+        int len = 100000;
+        Integer[] arr = new Integer[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = random.nextInt(i+1);
+        }
+
+        arr = insertionSort.insertionSort(arr);
+
+        for (Integer i : arr){
+            System.out.print(i+"\t");
+        }
 
     }
 }
