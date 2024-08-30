4a93f84f4db6db207540f5877984baefe2707fe2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-30 19:03:20 +0300

    Commit message >>>>> working on the generic insertion sort. <<<<<

--- HSortAlgorithm/src/InsertionSort/InsertionSort.java
+++ HSortAlgorithm/src/InsertionSort/InsertionSort.java
@@ -1,4 +1,16 @@
 package InsertionSort;
 
-public class InsertionSort {
+public class InsertionSort<T extends Comparable<T>>   {
+
+    public T[] insertionSort(T[] t){
+
+        for (int i = 0; i < t.length; i++) {
+            for (int j = i+1; j >= 0 ; j--) {
+                if (t[i].compareTo(t[j]) < 0){
+
+                }
+            }
+        }
+        return null;
+    }
 }
--- HSortAlgorithm/src/Main.java
+++ HSortAlgorithm/src/Main.java
@@ -1,7 +1,10 @@
+import InsertionSort.InsertionSort;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        InsertionSort<Double> insertionSort = new InsertionSort<Double>();
 
     }
 }
