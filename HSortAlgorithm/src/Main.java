7e7b981a241f75d1b386cdb03f82a265ba1ded6a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-08-31 06:48:41 +0300

    Commit message >>>>> working on h sort <<<<<

--- HSortAlgorithm/src/Main.java
+++ HSortAlgorithm/src/Main.java
@@ -2,8 +2,6 @@ import InsertionSort.InsertionSort;
 
 import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
