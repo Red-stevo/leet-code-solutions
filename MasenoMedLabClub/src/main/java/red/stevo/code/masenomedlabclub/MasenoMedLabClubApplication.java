2b1a50d79f0704f99548c0fe41d84063e8e770b4
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-09-04 18:13:41 +0300

    Commit message >>>>> initialized the spring web project. <<<<<

---
+++ MasenoMedLabClub/src/main/java/red/stevo/code/masenomedlabclub/MasenoMedLabClubApplication.java
@@ -0,0 +1,13 @@
+package red.stevo.code.masenomedlabclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasenoMedLabClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasenoMedLabClubApplication.class, args);
+    }
+
+}
---
+++ MasenoMedLabClub/src/test/java/red/stevo/code/masenomedlabclub/MasenoMedLabClubApplicationTests.java
@@ -0,0 +1,13 @@
+package red.stevo.code.masenomedlabclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasenoMedLabClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
