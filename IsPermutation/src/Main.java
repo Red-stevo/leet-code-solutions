72831404a3927f8811496625a90197f96569b7b2
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-03 09:10:16 +0300

    Commit message >>>>> all done <<<<<

--- IsPermutation/src/Main.java
+++ IsPermutation/src/Main.java
@@ -1,13 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    /*Complexity analysis
+    * Space complexity : O(1) -> constant space complexity
+    * Time complexity : O(n) -> linear, where n is the length of the string
+    * */
     public static void main(String[] args) {
-        String hello = "abc";
-        String hey = "cba";
+        String str1 = "hello";
+        String str2 = "olehl";
 
-        Solution solution = new Solution();
-        boolean response = solution.isPermutation(hello, hey);
+        Main main = new Main();
+        System.out.println(main.isPermutation(str1, str2));
+    }
 
-        System.out.println("Response : "+response);
+    public boolean isPermutation(String str1, String str2) {
+        /*Check if the strings have equal lengths, if not return false.*/
+        if (str1.length() != str2.length()) return false;
+        /*Initialize variables to hold the string int-values-sum.*/
+        int str1Sum = 0, str2Sum = 0;
+
+        /*iterate through the String to get the sum of their characters.*/
+        for (int i = 0; i < str1.length(); i++) {
+            str1Sum += str1.charAt(i);
+            str2Sum += str2.charAt(i);
+        }
+
+        /*If the sums are equal, then the isPermutation is true else false*/
+        return str1Sum == str2Sum;
     }
 }
--- IsPermutation/src/Solution.java
+++ IsPermutation/src/Solution.java
@@ -1,18 +1,1 @@
-public class Solution {
 
-    public boolean isPermutation(String str1, String str2){
-        /*Check if the strings have equal lengths, if not return false.*/
-        if (str1.length() != str2.length()) return false;
-        /*Initialize variable to hold string int values sum.*/
-        int str1Sum = 0, str2Sum = 0;
-
-        /*iterate through the String to get the sum of their characters.*/
-        for (int i = 0; i < str1.length(); i++) {
-            str1Sum += str1.charAt(i);
-            str2Sum += str2.charAt(i);
-        }
-
-        /*If the sums are equal, then the isPermutation is true else false*/
-        return str1Sum == str2Sum;
-    }
-}
