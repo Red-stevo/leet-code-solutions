b9412044c06c75a587f2728d70dce5625f28a4e6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-10-11 09:00:52 +0300

    Commit message >>>>> bug boomed, HAHAHAH!!! <<<<<

--- IsPermutation/src/Main.java
+++ IsPermutation/src/Main.java
@@ -7,8 +7,8 @@ public class Main {
     * Time complexity : O(n) -> linear, where n is the length of the string
     * */
     public static void main(String[] args) {
-        String str1 = "ce";
-        String str2 = "dd";
+        String str1 = "stevemuish";
+        String str2 = "vmueisshet";
 
         Main main = new Main();
         System.out.println(main.isPermutation(str1, str2));
@@ -17,16 +17,22 @@ public class Main {
     public boolean isPermutation(String str1, String str2) {
         /*Check if the strings have equal lengths, if not return false.*/
         if (str1.length() != str2.length()) return false;
-        /*Initialize variables to hold the string int-values-sum.*/
-        int str1Sum = 0, str2Sum = 0;
+
+        /*Initialize variables to hold the string prime product.*/
+        long str1Prod = 1, str2Prod = 1;
+
+        int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
 
         /*iterate through the String to get the sum of their characters.*/
         for (int i = 0; i < str1.length(); i++) {
-            str1Sum += str1.charAt(i);
-            str2Sum += str2.charAt(i);
+            str1Prod *= primes[str1.charAt(i) - 97];
+            str2Prod *= primes[str2.charAt(i) - 97];
         }
 
         /*If the sums are equal, then the isPermutation is true else false*/
-        return str1Sum == str2Sum;
+        return str1Prod == str2Prod;
     }
 }
