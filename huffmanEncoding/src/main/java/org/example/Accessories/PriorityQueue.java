1010c955e17f2a0406338118fccf17b8db15b1de
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2025-01-10 21:59:04 +0300

    Commit message >>>>> worked on the priority queue <<<<<

--- huffmanEncoding/src/main/java/org/example/Accessories/PriorityQueue.java
+++ huffmanEncoding/src/main/java/org/example/Accessories/PriorityQueue.java
@@ -1,8 +1,7 @@
 package org.example.Accessories;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -12,7 +11,54 @@ public class PriorityQueue {
     List<Long> frequencies;
 
     public PriorityQueue(List<Long> frequencies) {
+        this.frequencies = frequencies;
+        subSort(frequencies);
+        System.out.println(Arrays.toString(frequencies.toArray()));
+    }
 
+    private void subSort(List<Long> frequencies) {
+        int i = frequencies.size()-1;
+        int parent = (i-1)/2;
+
+        while (parent >= 0){
+            if (frequencies.get(parent) > frequencies.get(i)){
+                Long temp = frequencies.get(i);
+                frequencies.set(i, frequencies.get(parent));
+                frequencies.set(parent, temp);
+
+                int j = i;
+                while (j * 2 <= frequencies.size()-1){
+                    int leftChild = j*2;
+                    int rightChild = j*2+1;
+
+                    if (rightChild <= frequencies.size()-1){
+                        if (frequencies.get(j) <= frequencies.get(leftChild) &&
+                                frequencies.get(j) <= frequencies.get(rightChild))
+                            break;
+
+                        if (frequencies.get(leftChild) <= frequencies.get(rightChild)){
+                            temp = frequencies.get(j);
+                            frequencies.set(j, frequencies.get(leftChild));
+                            frequencies.set(leftChild, temp);
+                            j = leftChild;
+                        }else {
+                            temp = frequencies.get(j);
+                            frequencies.set(j, frequencies.get(rightChild));
+                            frequencies.set(rightChild, temp);
+                            j = rightChild;
+                        }
+                    }else {
+                        if (frequencies.get(j) <= frequencies.get(leftChild)) break;
+                        temp = frequencies.get(j);
+                        frequencies.set(j, frequencies.get(leftChild));
+                        frequencies.set(leftChild, temp);
+                        j = leftChild;
+                    }
+                }
+            }else
+                --i;
+            parent = (i-1)/2;
+        }
     }
 
     public void putFrequencies(Long value){
--- huffmanEncoding/src/main/java/org/example/Main.java
+++ huffmanEncoding/src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.Accessories.PriorityQueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Long> numbers = new ArrayList<>(List.of(8L, 7L, 6L, 2L, 5L, 4L, 4L, 2L, 3L, 2L, 1L, 1L));
 
+        PriorityQueue priorityQueue = new PriorityQueue(numbers);
     }
 }
