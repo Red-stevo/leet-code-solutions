fb29253d326e917762f17819b3e5fe7b5c0d2fe0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2025-01-19 06:39:20 +0300

    Commit message >>>>> new code desogn, working great <<<<<

--- huffmanEncoding/src/main/java/org/example/Accessories/Huffman.java
+++ huffmanEncoding/src/main/java/org/example/Accessories/Huffman.java
@@ -1,6 +1,10 @@
 package org.example.Accessories;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Huffman {
 
     private IHuffman iHuffman;
@@ -9,6 +13,23 @@ public class Huffman {
         iHuffman = new IHuffman();
     }
 
+    public String huffmanEncodeText(String text){
+        StringCount stringCount = text1 -> {
+            HashMap<Character, CharacterData> freMap = new HashMap<>();
+            for(Character Char : text1.toCharArray()){
+                if (freMap.containsKey(Char))
+                    freMap.get(Char).setCharacterCount(freMap.get(Char).getCharacterCount() + 1L);
+                else freMap.put(Char, new CharacterData(Char, 1L));
+            }
+            return freMap;
+        };
+
+        List<CharacterData> data = new ArrayList<>();
+        stringCount.count(text).forEach((character, characterData) -> {data.add(characterData);});
+        PriorityQueue priorityQueue = new PriorityQueue(data);
 
 
+
+        return null;
+    }
 }
--- huffmanEncoding/src/main/java/org/example/Main.java
+++ huffmanEncoding/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.Accessories.CharacterData;
+import org.example.Accessories.Huffman;
 import org.example.Accessories.PriorityQueue;
 import org.example.Accessories.StringCount;
 
@@ -11,18 +12,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String text = "sssssssssssssssttttttttteeeeeeeeeepppppppppppppphhhhhhhhhhhhhhhhheeeeeeeeeennnnnnnnnnnnnnnnnnnn mmmmmmmmmmmuuuuiiiiiiiiirrrrrruuuuuu";
-        StringCount stringCount = text1 -> {
-            HashMap<Character, CharacterData> freMap = new HashMap<>();
-            for(Character Char : text1.toCharArray()){
-                if (freMap.containsKey(Char))
-                    freMap.get(Char).setCharacterCount(freMap.get(Char).getCharacterCount() + 1L);
-                else freMap.put(Char, new CharacterData(Char, 1L));
-            }
-            return freMap;
-        };
-
-        List<CharacterData> data = new ArrayList<>();
-        stringCount.count(text).forEach((character, characterData) -> {data.add(characterData);});
-        PriorityQueue priorityQueue = new PriorityQueue(data);
+        Huffman huffman = new Huffman();
+        huffman.huffmanEncodeText(text);
     }
 }
